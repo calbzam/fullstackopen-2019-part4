a7a25fd972ca33cabe2b075c9bea04292f5e45a9
calbzam <albazamanillo@gmail.com>
2020-04-11 20:29:53 +0200

    ex 4.4 and 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,32 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs
+      .map(blog => blog.likes)
+      .reduce((sum, item) => sum + item)
+}
+
+const favoriteBlog = (blogs) => {
+  let likes = 0
+  let favorite = {}
+  blogs.forEach(blog => {
+    if (blog.likes > likes)
+    {
+      likes = blog.likes
+      favorite = blog
+    }
+  })
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
   }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
