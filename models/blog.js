ff97e9c7805e9b4106bdf6b7bdcfc48564cfdee4
calbzam <albazamanillo@gmail.com>
2020-04-19 20:32:52 +0200

    ex 4.9

--- models/blog.js
+++ models/blog.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-  module.exports = Blog
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
